b7398fd6d3f3de57c5fc57ad3650d00874e3f4ad
Steven Velozo <steven@velozo.com>
2026-03-10 12:03:56 -0700

    fixed build for relative paths

--- .gulpfile-quackage.js
+++ .gulpfile-quackage.js
@@ -1,2 +1,2 @@
-require('/Users/stevenvelozo/Code/retold/modules/apps/ultravisor/node_modules/quackage/gulp/Quackage-Gulpfile.js');
-require('/Users/stevenvelozo/Code/retold/modules/apps/ultravisor/node_modules/quackage/gulp/Quackage-Gulpfile.js');
+require('/Users/steven/Code/retold/modules/apps/ultravisor/node_modules/quackage/gulp/Quackage-Gulpfile.js');
+require('/Users/steven/Code/retold/modules/apps/ultravisor/node_modules/quackage/gulp/Quackage-Gulpfile.js');
